a14487d458c3f3bcddc7d56af31af5d615b7438a
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-01-27 21:47:44 -0300

    Aula 3

---
+++ src/app/pages/user-add/user-add.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-user-add',
+  templateUrl: './user-add.page.html',
+  styleUrls: ['./user-add.page.scss'],
+})
+export class UserAddPage implements OnInit {
+
+  user : User = new User();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  salvar() {
+    console.log('Dados Salvos...',this.user)
+
+  }
+
+}
