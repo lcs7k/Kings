11dfc96323559ec75d114f073fbc22b8fdca2f0f
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2021-02-08 21:48:42 -0300

    aula 5

--- src/app/pages/user-add/user-add.page.ts
+++ src/app/pages/user-add/user-add.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-add',
@@ -10,14 +12,33 @@ export class UserAddPage implements OnInit {
 
   user : User = new User();
 
-  constructor() { }
+  constructor(private storage: Storage,
+    public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   salvar() {
+    try {
     console.log('Dados Salvos...',this.user)
+    this.storage.set('name', this.user.nome);
+    this.storage.set('email', this.user.email);
+    this.storage.set('senha', this.user.senha);
+    this.presentAlert()
+}catch (error){
+ console.error("erro ao salvar.",error);
+}
 
-  }
+}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      //subHeader: 'Subtitle',
+      message: 'Usuario Cadastrado Com Sucesso!!!.',
+      buttons: ['OK']
+    });
+
+}
 }
